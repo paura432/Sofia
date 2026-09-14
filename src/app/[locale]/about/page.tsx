ce58fb372c5d334110871200b53caa4ab1259a29
pau <pau.ramos@performanze.io>
2026-09-14 19:38:19 +0200

    perf: preload profile portrait

--- src/app/[locale]/about/page.tsx
+++ src/app/[locale]/about/page.tsx
@@ -96,6 +96,7 @@ export default async function AboutPage() {
                 <Image
                   alt={profile(portrait.altKey)}
                   height={portrait.height}
+                  preload
                   sizes="(max-width: 699px) 100vw, 460px"
                   src={portrait.src}
                   style={{
